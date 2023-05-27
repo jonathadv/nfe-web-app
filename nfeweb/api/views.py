91fdfc04820b750523d8a29485c0c4845e82397c
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2023-05-27 12:33:54 -0300

    add 'Scan another NFe' link to scan result page

--- nfeweb/api/views.py
+++ nfeweb/api/views.py
@@ -47,16 +47,13 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = ProductDbModel.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["product_type__category__title", "metric_unit", "barcode"]
-
-    def get_queryset(self):
-        if "no_category" in self.request.query_params:
-            return self.queryset.filter(category=None)
-
-        if "no_product_type" in self.request.query_params:
-            return self.queryset.filter(product_type=None)
-
-        return super().get_queryset()
+    filterset_fields = [
+        "product_type__category__title",
+        "metric_unit",
+        "barcode",
+        "product_type",
+        "product_type__category",
+    ]
 
 
 class ProductCategoryViewSet(viewsets.ModelViewSet):
@@ -107,10 +104,15 @@ class NfeScanResult(APIView, mixins.UpdateModelMixin):
     template_name = "nfe.html"
 
     def get(self, request):
-        queryset = NfeDbModel.objects.get(id=request.GET.get("id"))
+        if nfe_id := request.GET.get("id"):
+            queryset = NfeDbModel.objects.get(id=nfe_id)
+        else:
+            queryset = NfeDbModel.objects.none()
+
         uncategorized_products_quantity = len(
             ProductDbModel.objects.filter(product_type__category__isnull=True)
         )
+
         return Response(
             {
                 "queryset": queryset,
